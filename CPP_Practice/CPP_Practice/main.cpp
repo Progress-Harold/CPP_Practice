e087f902b4059a27d1ab5f9f9600030c2f7d24aa
Lee Davis <harold_davis@icloud.com>
2021-05-15 09:52:35 -0700

    Doc

---
+++ CPP_Practice/CPP_Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_Practice
+//
+//  Created by Lee Davis on 5/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
