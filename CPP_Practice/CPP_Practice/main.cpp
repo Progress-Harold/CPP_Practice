4068d7296456a7c3000d62317e2c0e4264a258cc
Lee Davis <harold_davis@icloud.com>
2021-05-15 11:16:17 -0700

    pointers and references

--- CPP_Practice/CPP_Practice/main.cpp
+++ CPP_Practice/CPP_Practice/main.cpp
@@ -9,12 +9,26 @@
 
 int main()
 {
-
-    int x;
-    x = 42;
-
-    printf("x is %d\n", x);
-    printf("x is %d\n", x = 73);
+    int x = 42;
+    int * ip = &x;
+    int & y = x;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n\n", *ip);
+    
+    y = 73;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n\n", *ip);
+    
+    int z = 7;
+    ip = &z;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n", *ip);
     
     return 0;
 }
@@ -46,6 +60,7 @@ int main()
  
  */
 
+// Statments:
 int statment()
 {
 
@@ -54,6 +69,77 @@ int statment()
 
     printf("x is %d\n", x);
     printf("x is %d\n", x = 73);
+    
+    return 0;
+}
+
+// Variables:
+int variable()
+{
+
+    int i;
+    i = 7;
+    
+    printf("value is %d\n", i);
+    
+    return 0;
+}
+
+// Pointers:
+/*
+ int x;
+ x = 1;
+ int y = x; // just assigns the value
+ 
+ & = address operator
+ 
+ int * ip;
+ ip = &x; // assigns the address or refereance
+ y = *ip; // copies the value to the reference of y
+ */
+
+int pointers()
+{
+    int x = 7;
+    int y = 42;
+    int * ip = &x;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n", *ip);
+    
+    x = 73;
+    printf("The value of ip is %d\n", *ip);
+    
+    return 0;
+}
+
+// References:
+// Cant change a reference
+// Always const references
+
+int references()
+{
+    int x = 42;
+    int * ip = &x;
+    int & y = x;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n\n", *ip);
+    
+    y = 73;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n\n", *ip);
+    
+    int z = 7;
+    ip = &z;
+    
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    printf("The value of ip is %d\n", *ip);
     
     return 0;
 }
