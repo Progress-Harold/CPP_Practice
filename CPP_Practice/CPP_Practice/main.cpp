0c3758d4a55b7d0f4c4f6d372e591f0b7bcd0dd7
Lee Davis <harold_davis@icloud.com>
2021-05-15 10:25:34 -0700

    Setup and intro

--- CPP_Practice/CPP_Practice/main.cpp
+++ CPP_Practice/CPP_Practice/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
+#include <cstdio>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    puts("Hello, World!");
+    
     return 0;
 }
+
+/*
+ What is C++?
+ 
+ c with classes
+ developed by Bjarne stroustrup
+ extended from c
+ consistently one of the top 5 ;anguages
+
+ Apps
+ OS
+ Firmware
+
+ STL - Standared Template library
+ 
+ 
+ Tool Chain
+ 
+ Compiled language
+ 
+                            * Compiling *
+ Source Code -> | Preprocessor -> Compiler -> Linker | -> Run (executable)
+ 
+ --------------------------------------------------
+ Parameters in main(int argc, const char * argv[])
+ 
+ */
